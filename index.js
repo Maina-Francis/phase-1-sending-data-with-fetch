7fd82609b6acdd646731fe45ea0d63c1f0773be7
Francis Maina <sebastianfrancix@gmail.com>
2022-08-30 02:53:10 +0300

    makes a POST request to users with a name and email

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your code here
+//makes a POST request to users with a name and email
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  });
+}
