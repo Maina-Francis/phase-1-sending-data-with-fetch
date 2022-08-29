0882ae1eeb623fa078cc405f9ada30208ed431f5
Francis Maina <sebastianfrancix@gmail.com>
2022-08-30 02:55:39 +0300

    handles the POST request response, retrieves the new id value and appends it to the dom

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //makes a POST request to users with a name and email
 function submitData(name, email) {
-  return fetch("http://localhost:3000/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ name, email }),
-  });
+  return (
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ name, email }),
+    })
+      //handles the POST request response, retrieves the new id value and appends it to the DOM
+      .then((response) => response.json())
+      .then(function (data) {
+        return (document.body.innerHTML = data["id"]);
+      })
+  );
 }
